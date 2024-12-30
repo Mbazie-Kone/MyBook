91202387d50fb51085a8a63c7486c45b4cde6ad6
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 15:24:55 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/Product.java
+++ src/main/java/com/mbaziekone/mybook/model/Product.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -33,14 +37,22 @@ public class Product implements Serializable {
 	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
 	
+	@Column(name = "image_url")
 	private String imagePath;
 	
 	private boolean active;
 	
+	@Column(name = "units_in_stock")
 	private int unitsInStock;
 	
+	@Column(name = "date_created")
+	@CreationTimestamp
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dateCreated;
 	
+	@Column(name = "last_update")
+	@UpdateTimestamp
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date lastUpdate;
 	
 	private ProductCategory category;
