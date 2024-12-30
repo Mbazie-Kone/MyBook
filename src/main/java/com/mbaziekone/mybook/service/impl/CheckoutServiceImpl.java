8aaa84cd84befe0c25ef6510796f3b4850cdba77
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 19:08:23 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/service/impl/CheckoutServiceImpl.java
+++ src/main/java/com/mbaziekone/mybook/service/impl/CheckoutServiceImpl.java
@@ -1,8 +1,13 @@
 package com.mbaziekone.mybook.service.impl;
 
+import java.util.Set;
+
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.stereotype.Service;
 
+import com.mbaziekone.mybook.model.Customer;
 import com.mbaziekone.mybook.model.Order;
+import com.mbaziekone.mybook.model.OrderItem;
 import com.mbaziekone.mybook.model.dto.Purchase;
 import com.mbaziekone.mybook.model.dto.PurchaseResponse;
 import com.mbaziekone.mybook.repository.CustomerRepository;
@@ -26,12 +31,24 @@ public class CheckoutServiceImpl implements CheckoutService {
 		// retrieve the order info from dto
 		Order order = purchase.getOrder();
 		
-		// generate tracking numbe
+		// generate tracking number
 		String orderTrackingNumber = generateOrderTrackingNumber();
 		order.setOrderTrackingNumber(orderTrackingNumber);
 		
+		// populate order with orderItems
+		Set<OrderItem> orderItems = purchase.getOrderItems();
+		orderItems.forEach(Item -> order.add(Item));
+		
+		// populate order with billingAddress and shippingAddress
+		order.setBillingAddress(purchase.getBillingAddress());
+		order.setShippingAddress(purchase.getShippingAddress());
 		
 		
+		Customer customer = purchase.getCustomer();
+		customer.add(order);
+		
+		
+		customerRepository.save(customer);
 		
 		return null;
 	}
