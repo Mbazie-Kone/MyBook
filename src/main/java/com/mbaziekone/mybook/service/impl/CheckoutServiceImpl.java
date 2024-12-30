90b0b347095b8f5a1d1cbf13671a04cda9203312
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 18:58:36 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/service/impl/CheckoutServiceImpl.java
+++ src/main/java/com/mbaziekone/mybook/service/impl/CheckoutServiceImpl.java
@@ -1,16 +1,38 @@
 package com.mbaziekone.mybook.service.impl;
 
+import org.springframework.stereotype.Service;
+
+import com.mbaziekone.mybook.model.Order;
 import com.mbaziekone.mybook.model.dto.Purchase;
 import com.mbaziekone.mybook.model.dto.PurchaseResponse;
+import com.mbaziekone.mybook.repository.CustomerRepository;
 import com.mbaziekone.mybook.service.CheckoutService;
 
+import jakarta.transaction.Transactional;
+
+@Service
 public class CheckoutServiceImpl implements CheckoutService {
 	
+	private CustomerRepository customerRepository;
 	
+	public CheckoutServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
+	@Transactional
 	public PurchaseResponse placeOrder(Purchase purchase) {
-		// TODO Auto-generated method stub
+		
+		// retrieve the order info from dto
+		Order order = purchase.getOrder();
+		
+		// generate tracking numbe
+		String orderTrackingNumber = generateOrderTrackingNumber();
+		order.setOrderTrackingNumber(orderTrackingNumber);
+		
+		
+		
+		
 		return null;
 	}
 
