d9bb814da8d379e191295fa7a72eeb8040cc5185
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 15:40:06 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/MyBookApplication.java
+++ src/main/java/com/mbaziekone/mybook/MyBookApplication.java
@@ -2,8 +2,10 @@ package com.mbaziekone.mybook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mbaziekone.mybook")
 public class MyBookApplication {
 
 	public static void main(String[] args) {
