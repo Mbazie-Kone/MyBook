cc12658977c8f4d8ae4fa8147f5fcb4ee31695ac
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 13:31:17 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/config/DataRestConfig.java
+++ src/main/java/com/mbaziekone/mybook/config/DataRestConfig.java
@@ -1,5 +1,13 @@
 package com.mbaziekone.mybook.config;
 
-public class DataRestConfig {
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class DataRestConfig {
+	
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		
+	}
 }
