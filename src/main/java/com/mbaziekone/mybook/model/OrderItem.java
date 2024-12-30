c5fabdf6e1ccf89e6760c88806a4c376f4b69a5d
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 16:23:16 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/OrderItem.java
+++ src/main/java/com/mbaziekone/mybook/model/OrderItem.java
@@ -11,9 +11,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "order_items")
+@Getter
+@Setter
+@ToString
 public class OrderItem implements Serializable{
 
 	private static final long serialVersionUID = 1L;
