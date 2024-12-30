b4cce9a742cd9c62b118eb3cf0d6760142f856f7
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 17:49:52 +0100

    Updates

---
+++ src/main/java/com/mbaziekone/mybook/model/Country.java
@@ -0,0 +1,37 @@
+package com.mbaziekone.mybook.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "countries")
+@Getter
+@Setter
+public class Country implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String code;
+	
+	private String name;
+	
+	@OneToMany(mappedBy = "country")
+	@JsonIgnore
+	private List<State> states;
+
+}
