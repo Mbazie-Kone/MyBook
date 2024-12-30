77d9d0268c4cc8f4c88a2af7e52357d535105659
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 13:54:00 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/Product.java
+++ src/main/java/com/mbaziekone/mybook/model/Product.java
@@ -2,7 +2,12 @@ package com.mbaziekone.mybook.model;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,5 +15,10 @@ import jakarta.persistence.Table;
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("id")
+	private Long id;
 
 }
