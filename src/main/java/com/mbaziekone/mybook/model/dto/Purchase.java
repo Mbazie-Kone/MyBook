d84c0f0834a356c98b755c04628fc53c1cb4d98a
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 16:06:48 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/dto/Purchase.java
+++ src/main/java/com/mbaziekone/mybook/model/dto/Purchase.java
@@ -1,9 +1,29 @@
 package com.mbaziekone.mybook.model.dto;
 
 
-import com.mbaziekone.mybook.model.Customer;
+import java.util.Set;
 
+import com.mbaziekone.mybook.model.Address;
+import com.mbaziekone.mybook.model.Customer;
+import com.mbaziekone.mybook.model.Order;
+import com.mbaziekone.mybook.model.OrderItem;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Purchase {
 	
 	private Customer customer;
+	
+	private Address shippingAddress;
+	
+	private Address billingAddress;
+	
+	private Order order;
+	
+	private Set<OrderItem> orderItems;
 }
