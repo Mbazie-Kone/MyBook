0824a6f57a4fd481b6f6689408b7975b7cbce204
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 22:30:30 +0100

    Updates

--- front_end/MyBook/src/app/app-routing.module.ts
+++ front_end/MyBook/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
-  {path : 'login', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
