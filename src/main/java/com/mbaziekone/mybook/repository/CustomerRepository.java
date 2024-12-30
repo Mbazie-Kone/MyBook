c1bc5c87f47c8cd6b585f41e210c023ae01186fc
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 18:51:44 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/repository/CustomerRepository.java
+++ src/main/java/com/mbaziekone/mybook/repository/CustomerRepository.java
@@ -1,5 +1,9 @@
 package com.mbaziekone.mybook.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mbaziekone.mybook.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
