b98b970ea75f6b436a2105d67734205468131a47
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 16:21:24 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/OrderItem.java
+++ src/main/java/com/mbaziekone/mybook/model/OrderItem.java
@@ -1,5 +1,40 @@
 package com.mbaziekone.mybook.model;
 
-public class OrderItem {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+	
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+	
+	private int quantity;
+	
+	@Column(name = "product_id")
+	private Long productId;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
 
 }
