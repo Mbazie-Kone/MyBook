20d696a3a3b8df3e3cf9c751f7ffafb623455682
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 13:50:05 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/Product.java
+++ src/main/java/com/mbaziekone/mybook/model/Product.java
@@ -1,5 +1,14 @@
 package com.mbaziekone.mybook.model;
 
-public class Product {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 }
--- src/main/java/com/mbaziekone/mybook/model/ProductCategory.java
+++ src/main/java/com/mbaziekone/mybook/model/ProductCategory.java
@@ -1,5 +1,18 @@
 package com.mbaziekone.mybook.model;
 
-public class ProductCategory {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product_categories")
+@Getter
+@Setter
+public class ProductCategory implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 }
