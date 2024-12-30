bd3d151a5b80e3b85933e19d445cdf1f0a1444c7
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 16:47:38 +0100

    Updates

--- src/main/java/com/mbaziekone/mybook/model/Address.java
+++ src/main/java/com/mbaziekone/mybook/model/Address.java
@@ -1,5 +1,45 @@
 package com.mbaziekone.mybook.model;
 
-public class Address {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@ToString
+public class Address implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private String country;
+	
+	@Column(name = "zip_code")
+	private String zipCode;
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private Order order;
 
 }
