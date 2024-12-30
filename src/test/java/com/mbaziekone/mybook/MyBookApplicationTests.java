8f3aedef23f8ad60c0f378fe3286a0dbdb21a321
M'bazie Kone <mbazie89@gmail.com>
2024-12-30 12:41:25 +0100

    First commit

---
+++ src/test/java/com/mbaziekone/mybook/MyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbaziekone.mybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
