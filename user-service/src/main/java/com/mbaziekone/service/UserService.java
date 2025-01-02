a49a904a0c3e27f6eff8efc3cd1b7e7c3450620d
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 14:38:53 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/service/UserService.java
+++ user-service/src/main/java/com/mbaziekone/service/UserService.java
@@ -1,5 +1,11 @@
 package com.mbaziekone.service;
 
+import java.util.List;
+
+import com.mbaziekone.model.User;
+
 public interface UserService {
+	
+	public List<User> getAllUsers();
 
 }
--- user-service/src/main/java/com/mbaziekone/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/mbaziekone/service/impl/UserServiceImpl.java
@@ -1,7 +1,16 @@
 package com.mbaziekone.service.impl;
 
+import java.util.List;
+
+import com.mbaziekone.model.User;
 import com.mbaziekone.service.UserService;
 
 public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
