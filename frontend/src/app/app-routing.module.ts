27f9cbf1cd2b13d636ab2f2a2349b5f33db796a0
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 14:19:42 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
-import { UserListComponent } from './users/user-list/user-list.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'users', component: UserListComponent},
+  {
+    path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule) // users-routing.module.ts import
+  },
   {path: '**', component: ErrorComponent}
 ];
 
--- frontend/src/app/users/users-routing.module.ts
+++ frontend/src/app/users/users-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserListComponent}, // Default route
+  {path: ':id', component: UserDetailsComponent} // Users route details
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
