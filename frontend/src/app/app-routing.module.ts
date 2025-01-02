2c397f1baffe69d09ae5dda8f577df08a22fefba
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 16:39:44 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 import { UserListComponent } from './components/users/user-list/user-list.component';
 import { UserDetailsComponent } from './components/users/user-details/user-details.component';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'user', component: UserListComponent},
-  {path: 'user/name', component: UserDetailsComponent},
+  {path: 'user/id', component: UserDetailsComponent},
+  {path: 'create-user', component: AddUserComponent},
   {path: '**', component: ErrorComponent}
 ];
 
