176588db9a4b4b1c9f1cda437cd09acfd1fdcdfe
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 16:26:59 +0100

    Updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { UserDetailsComponent } from './components/users/user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ErrorComponent,
+    AddUserComponent,
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
