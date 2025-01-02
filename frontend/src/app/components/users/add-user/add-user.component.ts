c164b97e6713b67d3f6121f4d1afaee0f6a873b0
M'bazie Kone <mbazie89@gmail.com>
2025-01-03 00:14:21 +0100

    Updates

--- frontend/src/app/components/users/add-user/add-user.component.ts
+++ frontend/src/app/components/users/add-user/add-user.component.ts
@@ -5,26 +5,26 @@ import { UsersService } from '../../../services/users.service';
 @Component({
   selector: 'app-add-user',
   standalone: false,
-  
+
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
-  user: User = {name: ''};
+  user: User = { name: '' };
 
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) { }
 
   saveUser(): void {
     this.userService.createUser(this.user).subscribe({
       next: (response) => {
         console.log('User saved successfully:', response);
       },
-    error: (error) => {
+      error: (error) => {
         console.error('Error saving user:', error);
       },
-    complete: () => {
-      console.log('Request completed');
-    },
-  });
+      complete: () => {
+        console.log('Request completed');
+      },
+    });
   }
 }
