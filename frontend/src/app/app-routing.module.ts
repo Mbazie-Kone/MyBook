d5b2b85d681775049b6a2518f83a80b21f4d54bf
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 16:36:05 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { UserDetailsComponent } from './components/users/user-details/user-details.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {
-    path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule) // users-routing.module.ts import
-  },
+  {path: 'user', component: UserListComponent},
+  {path: 'user/name', component: UserDetailsComponent},
   {path: '**', component: ErrorComponent}
 ];
 
