d7860f727304996d1c3ccb9b098544f9a863cc95
M'bazie Kone <mbazie89@gmail.com>
2025-01-02 13:43:09 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/model/User.java
+++ user-service/src/main/java/com/mbaziekone/model/User.java
@@ -3,6 +3,9 @@ package com.mbaziekone.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +20,10 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	private String name;
 
 }
